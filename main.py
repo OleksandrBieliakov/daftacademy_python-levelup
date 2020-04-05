4437daf8e9f638b881ce1027c415de0055988537
Oleksandr Bieliakov <abel9k@gmail.com>
2020-04-05 16:41:54 +0200

    Homework 1 task 1 implemented

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from pydantic import BaseModel
 
-@app.get("/")
+app = FastAPI()
+# to see what funny will come
+app.counter = 0
+
+
+class HelloResp(BaseModel):
+    message: str
+
+
+class GiveMeSomethingRq(BaseModel):
+    first_key: str
+
+
+class GiveMeSomethingResp(BaseModel):
+    received: dict
+    constant_data: str = "python jest super"
+
+
+@app.get("/", response_model=HelloResp)
 def root():
-    return {"message": "Hello World"}
+    return HelloResp(message="Hello World during the coronavirus pandemic!")
+
+
+@app.post("/giveme", response_model=GiveMeSomethingResp)
+def receive_something(rq: GiveMeSomethingRq):
+    return GiveMeSomethingResp(received=rq.dict())
+
+
+@app.get('/counter')
+def counter():
+    app.counter += 1
+    return app.counter
+
+
+@app.get("/hello/{name}", response_model=HelloResp)
+async def read_item(name: str):
+    return HelloResp(message=f"Hello {name}")
+
+
--- test_main.py
+++ test_main.py
@@ -1,6 +1,8 @@
 from fastapi.testclient import TestClient
-
 from main import app
+from main import HelloResp
+from json import dumps
+import pytest
 
 client = TestClient(app)
 
@@ -8,4 +10,26 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {"message": "Hello World during the coronavirus pandemic!"}
+
+
+@pytest.mark.parametrize("name", ['Zenek', 'Marek', 'Alojzy Niezdąży'])
+def test_hello_name(name):
+    response = client.get(f"/hello/{name}")
+    assert response.status_code == 200
+    assert dumps(response.json()) == dumps(HelloResp(message=f"Hello {name}").__dict__)
+
+
+def test_counter():
+    response = client.get("/counter")
+    assert response.status_code == 200
+    assert "1" == response.text
+    response = client.get("/counter")
+    assert response.status_code == 200
+    assert "2" == response.text
+
+
+def test_receive_something():
+    response = client.post("/giveme", json={'first_key': 'some_value'})
+    assert response.json() == {"received": {'first_key': 'some_value'},
+                             "constant_data": "python jest super"}
